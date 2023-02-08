555d112b9fb580353660584b7a51246899aaa507
Nodaa Gaji <c0d3r.nodiru.gaji@gmail.com>
2023-02-08 12:12:41 +0300

    Loading currencies and wallets
    Signed-off-by: Nodaa Gaji <c0d3r.nodiru.gaji@gmail.com>

--- accountant/routes/main.py
+++ accountant/routes/main.py
@@ -12,26 +12,36 @@ def current_logged_user():
 
 @app.route('/', methods=['GET'])
 def index():
-    current_user = current_logged_user()
+  current_user = current_logged_user()
 
-    with sqlite3.connect(tables.get_db_path(app)) as conn:
-      curr = conn.cursor()
+  # XXX: VOCABULARY
+  # us2 = user subscribed to
+  with sqlite3.connect(tables.get_db_path(app)) as conn:
+    curr = conn.cursor()
 
-      user_subscription = query.Filters(curr).get_user_filter(
-        current_user, 'user'
-      )
-      all_users = query.User(curr).get_all()
-      users_schedules = query.Schedules(curr).get_users_schedules(
-        user_subscription
-      )
-      users_todo = query.Todo(curr).get_users_todo(
-        user_subscription
-      )
-
-    return render_template(
-      'main.html',
-      user_subscription=user_subscription,
-      all_users=all_users,
-      users_schedules=users_schedules,
-      users_todo=users_todo
+    us2_users = query.Filter(curr).get_user_filter(
+      current_user, 'user'
     )
+    all_users = query.User(curr).get_all()
+    users_schedules = query.Schedules(curr).get_users_schedules(
+      us2_users
+    )
+    users_todos = query.Todo(curr).get_users_todo(
+      us2_users
+    )
+    all_currencies = query.Currency(curr).get_all()
+    us2_currencies = query.Filter(curr).get_user_filter(
+      current_user, 'currency'
+    )
+    all_wallets = query.Wallet(curr).get_all()
+
+  return render_template(
+    'main.html',
+    us2_users=us2_users,
+    all_users=all_users,
+    users_schedules=users_schedules,
+    users_todos=users_todos,
+    all_currencies=all_currencies,
+    us2_currencies=us2_currencies,
+    all_wallets=all_wallets
+  )
